8ca4f4b6cb4cd3572da00bb952bc9d824254ed1b
handirachmawan <mail.handira@gmail.com>
2022-01-08 00:04:05 +0700

    Implements update and delete category

--- app/src/main/java/id/handiism/mvvm/UpdateDeleteCategoryDialogFragment.kt
+++ app/src/main/java/id/handiism/mvvm/UpdateDeleteCategoryDialogFragment.kt
@@ -3,23 +3,81 @@ package id.handiism.mvvm
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.text.Editable
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatDialogFragment
+import androidx.lifecycle.ViewModelProvider
 import id.handiism.mvvm.databinding.UpdateDeleteCategoryDialogFragmentBinding
 
-class UpdateDeleteCategoryDialogFragment(category: Category) : AppCompatDialogFragment() {
+class UpdateDeleteCategoryDialogFragment : AppCompatDialogFragment(), View.OnClickListener {
     private lateinit var binding: UpdateDeleteCategoryDialogFragmentBinding
     private lateinit var builder: AlertDialog.Builder
     private lateinit var alertDialog: AlertDialog
-    var detail: Category = category
-
+    private lateinit var viewModel: UpdateDeleteCategoryDialogViewModel
+    private var category: Category? = null
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         builder = AlertDialog.Builder(activity)
         binding = UpdateDeleteCategoryDialogFragmentBinding.inflate(LayoutInflater.from(context))
         builder.setView(binding.root)
-        binding.etCategoryName.setText(detail.name)
+        initView()
+        binding.btnUpdate.setOnClickListener(this)
+        binding.btnDelete.setOnClickListener(this)
         alertDialog = builder.create()
+        alertDialog.setOnDismissListener {
+            (activity as MainActivity).updateDataSet()
+        }
         return alertDialog
+    }
+
+    private fun initUpdateCategoryViewModel() {
+        viewModel = ViewModelProvider(this)[UpdateDeleteCategoryDialogViewModel::class.java]
+        viewModel.getUpdateCategoryObservable().observe(this, fun(it: Category?) {
+            Log.d("UpdateCategoryViewModel", "category: $it")
+            if (it != null) {
+                category = it
+            }
+        })
+        Log.d(
+            "UpdateCategoryViewModel",
+            "params: category($category!!.id , ${binding.etCategoryName.text})"
+        )
+        viewModel.updateCategory(category!!.id, RequestBody(binding.etCategoryName.text.toString()))
+    }
+
+    private fun initDeleteCategoryViewModel() {
+        viewModel = ViewModelProvider(this)[UpdateDeleteCategoryDialogViewModel::class.java]
+        viewModel.getDeleteCategoryObservable().observe(this, fun(_: Category?) {
+            Log.d("DeleteCategoryViewModel", "params: category(${category!!.id})")
+        })
+        viewModel.deleteCategory(category!!.id)
+    }
+
+    private fun initView() {
+        this.category = arguments?.getParcelable("category")
+        Log.d("initView", "category: ${this.category}")
+        binding.etCategoryId.text = Editable.Factory().newEditable(category?.id.toString())
+        binding.etCategoryId.isFocusable = false
+        binding.etCategoryId.isClickable = false
+        binding.etCategoryId.isLongClickable = false
+        binding.etCategoryId.isCursorVisible = false
+        binding.etCategoryName.text = Editable.Factory().newEditable(category?.name)
+    }
+
+    override fun onClick(v: View?) {
+        if (v == binding.btnUpdate) {
+            initUpdateCategoryViewModel()
+            Toast.makeText(context, "Category Changed Successfully", Toast.LENGTH_SHORT).show()
+            dismiss()
+            (activity as MainActivity).updateDataSet()
+        } else if (v == binding.btnDelete) {
+            initDeleteCategoryViewModel()
+            Toast.makeText(context, "Category Deleted Successfully", Toast.LENGTH_SHORT).show()
+            dismiss()
+            (activity as MainActivity).updateDataSet()
+        }
     }
 }
--- app/src/main/java/id/handiism/mvvm/UpdateDeleteCategoryDialogViewModel.kt
+++ app/src/main/java/id/handiism/mvvm/UpdateDeleteCategoryDialogViewModel.kt
@@ -1,7 +1,66 @@
 package id.handiism.mvvm
 
+import android.content.ContentValues.TAG
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class UpdateDeleteCategoryDialogViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    var updateCategoryLiveData: MutableLiveData<Category?> = MutableLiveData()
+    private var deleteCategoryLiveData: MutableLiveData<Category?> = MutableLiveData()
+
+
+    fun getDeleteCategoryObservable(): MutableLiveData<Category?> {
+        return deleteCategoryLiveData
+    }
+
+    fun getUpdateCategoryObservable(): MutableLiveData<Category?> {
+        return updateCategoryLiveData
+    }
+
+    fun updateCategory(id: Int, requestBody: RequestBody) {
+        val retroInstance = RetroInstance.getRetroInstance().create(RetroService::class.java)
+        val call = retroInstance.updateCategory(id, requestBody)
+        call.enqueue(object : Callback<ResponseBody> {
+            override fun onResponse(
+                call: Call<ResponseBody>,
+                response: Response<ResponseBody>
+            ) {
+                if (response.isSuccessful) {
+                    updateCategoryLiveData.postValue(response.body()?.data?.get(0))
+                } else {
+                    updateCategoryLiveData.postValue(null)
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                updateCategoryLiveData.postValue(null)
+                Log.d(TAG, "onFailure: $t")
+            }
+
+        })
+    }
+
+    fun deleteCategory(id: Int) {
+        val retroInstance = RetroInstance.getRetroInstance().create(RetroService::class.java)
+        val call = retroInstance.deleteCategory(id)
+        call.enqueue(object : Callback<ResponseBody> {
+            override fun onResponse(
+                call: Call<ResponseBody>,
+                response: Response<ResponseBody>
+            ) {
+                if (response.isSuccessful) {
+                    Log.d(TAG, "onResponse: pass")
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                Log.d(TAG, "onFailure: $t")
+            }
+
+        })
+    }
 }
