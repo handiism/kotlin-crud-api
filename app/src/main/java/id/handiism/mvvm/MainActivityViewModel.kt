4df96e7a3a67bb835531a84be39733d6f18fe3b9
handirachmawan <mail.handira@gmail.com>
2022-01-08 00:03:01 +0700

    Add find by id implementation and reformat code

--- app/src/main/java/id/handiism/mvvm/MainActivityViewModel.kt
+++ app/src/main/java/id/handiism/mvvm/MainActivityViewModel.kt
@@ -1,6 +1,5 @@
 package id.handiism.mvvm
 
-import android.content.ContentValues.TAG
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -9,29 +8,66 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivityViewModel : ViewModel() {
-    var recyclerListData: MutableLiveData<List<Category>?> = MutableLiveData()
+    var allCategoryLiveData: MutableLiveData<List<Category>?> = MutableLiveData()
+    private var categoryLiveData: MutableLiveData<Category?> = MutableLiveData()
+    var status = MutableLiveData<Boolean>()
+
 
     fun getAllCategoryObservable(): MutableLiveData<List<Category>?> {
-        return recyclerListData
+        return allCategoryLiveData
+    }
+
+    fun getCategoryByIdObservable(): MutableLiveData<Category?> {
+        return categoryLiveData
     }
 
     fun getAllCategory() {
         val retroInstance = RetroInstance.getRetroInstance().create(RetroService::class.java)
         val call = retroInstance.getAllCategory()
         call.enqueue(object : Callback<ResponseBody> {
-            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
-                Log.d(TAG, "onResponse: ${response.headers()}")
-                Log.d(TAG, "onResponse: ${response.body()}")
+            override fun onResponse(
+                call: Call<ResponseBody>,
+                response: Response<ResponseBody>
+            ) {
+                Log.d("GetAllCategory", "onResponse: true")
+                Log.d("GetAllCategory", "onResponse: ${response.body()}")
                 if (response.isSuccessful) {
-                    recyclerListData.postValue(response.body()?.data)
+                    allCategoryLiveData.postValue(response.body()?.data)
                 }
             }
 
             override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-                recyclerListData.postValue(null)
-                Log.d(TAG, "onFailure: $t")
+                allCategoryLiveData.postValue(null)
+                Log.d("GetAllCategory", "onFailure: $t")
             }
         })
+    }
 
+    fun getCategoryById(id: Int) {
+        val retroInstance = RetroInstance.getRetroInstance().create(RetroService::class.java)
+        val call = retroInstance.getCategoryById(id)
+        call.enqueue(object : Callback<ResponseBody> {
+            override fun onResponse(
+                call: Call<ResponseBody>,
+                response: Response<ResponseBody>
+            ) {
+                if (response.isSuccessful) {
+                    Log.d("GetCategoryById", "onResponse: true")
+                    Log.d("GetCategoryById", "responseBody: ${response.body()}")
+                    categoryLiveData.postValue(response.body()?.data?.get(0))
+                    status.value = true
+                } else {
+                    Log.d("GetCategoryById", "onResponse: false")
+                    categoryLiveData.postValue(null)
+                    status.value = false
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                categoryLiveData.postValue(null)
+                status.value = false
+                Log.d("GetCategoryById", "onFailure: $t")
+            }
+        })
     }
 }
